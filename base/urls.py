3783f6ea1e40844c629f402965e43da6df656185
Arad Afraz <threewestrick@gmail.com>
2023-07-14 16:11:33 +0330

    simple CRUD for Room

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("rooms/", views.rooms, name="rooms"),
     path("room/<int:pk>/", views.room, name="room"),
+    path("room/create/", views.create, name="create"),
+    path("room/<int:pk>/edit/", views.edit, name="edit"),
+    path("room/<int:pk>/delete/", views.delete, name="delete"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -16,3 +17,34 @@ def rooms(request):
 def room(request, pk):
     room_item = get_object_or_404(Room, pk=pk)
     return render(request, 'base/room.html', {'room': room_item})
+
+
+def create(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('base:index')
+    return render(request, 'base/room_form.html', {'form': form})
+
+
+def edit(request, pk):
+    room_item = get_object_or_404(Room, pk=pk)
+    form = RoomForm(instance=room_item)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room_item)
+        if form.is_valid():
+            form.save()
+            return redirect('base:index')
+
+    return render(request, 'base/room_form.html', {'form': form})
+
+
+def delete(request, pk):
+    room_item = get_object_or_404(Room, pk=pk)
+    if request.method == 'POST':
+        room_item.delete()
+        return redirect('base:index')
+    return render(request, 'base/delete.html', {'obj': room_item})
